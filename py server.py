08a69f2194699a8aee94b6afb5fb20c15849c336
Iniya-15 <iniya.me15@gmail.com>
2025-04-21 09:03:13 +0530

    Add files via upload

---
+++ py server.py
@@ -0,0 +1,18 @@
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+class CustomHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        # You can customize the response here
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"<html><body><h1>Hello, World!</h1></body></html>")
+
+def run(server_class=HTTPServer, handler_class=CustomHandler, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting server on port {port}...')
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
